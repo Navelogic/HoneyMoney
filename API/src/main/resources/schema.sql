ab88f7b3adc1ecbfa180122ffb75929cebe55a83
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-05 13:05:50 -0300

    Create schema.sql

---
+++ API/src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+# Author: Navelogic
+# Date: 05/06/2023
+# Version: 1.0
+
+/*
+ * Rules:
+    * 1. Table names are plural
+    * 2. Table names are lowercase
+    * 3. Table names are snake_case
+    * 4. Spell out id in table names (item_id, user_id, etc.)
+ */
