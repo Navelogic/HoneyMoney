8b183466a0f43c5443c80fd80fc242525089ff83
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-06 14:59:16 -0300

    Create Category.java

---
+++ API/src/main/java/br/com/honeymoney/api/model/Category.java
@@ -0,0 +1,48 @@
+package br.com.honeymoney.api.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorys")
+public class Category {
+    // Long
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // String
+    private String name;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Equals and HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return id.equals(category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
