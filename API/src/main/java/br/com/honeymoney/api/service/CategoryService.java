5f24ca52e8b2fe7d3a38865faca2c208899b368c
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-07 12:13:10 -0300

    Update CategoryService.java

--- API/src/main/java/br/com/honeymoney/api/service/CategoryService.java
+++ API/src/main/java/br/com/honeymoney/api/service/CategoryService.java
@@ -1,12 +1,14 @@
 package br.com.honeymoney.api.service;
 
 import br.com.honeymoney.api.dao.CategoryDAO;
+import br.com.honeymoney.api.event.ResourceCreatedEvent;
 import br.com.honeymoney.api.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
@@ -18,6 +20,9 @@ public class CategoryService {
     @Autowired
     private CategoryDAO categoryDAO;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     public ResponseEntity<?> findAll() {
         List<Category> categorys = categoryDAO.findAll();
         return !categorys.isEmpty() ? ResponseEntity.ok(categorys) : ResponseEntity.noContent().build();
@@ -28,6 +33,7 @@ public class CategoryService {
         return category != null ? ResponseEntity.ok(category) : ResponseEntity.notFound().build();
     }
 
+    @Transactional
     public ResponseEntity<Category> save(Category category, HttpServletResponse response) {
         // Verificar se já existe uma categoria com o mesmo nome
         Category existingCategory = categoryDAO.findByName(category.getName());
@@ -38,15 +44,12 @@ public class CategoryService {
 
         // Nenhuma categoria com o mesmo nome foi encontrada, prosseguir com o salvamento
         Category categorySaved = categoryDAO.save(category);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequestUri()
-                .path("/{id}")
-                .buildAndExpand(categorySaved.getId())
-                .toUri();
-        response.setHeader("Location", location.toASCIIString());
-        return ResponseEntity.created(location).body(categorySaved);
-    }
 
+        // Publicar o evento
+        publisher.publishEvent(new ResourceCreatedEvent(this, response, categorySaved.getId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(categorySaved);
+    }
 
     public ResponseEntity<?> delete(Long id) {
         Category category = categoryDAO.findById(id).orElse(null);
