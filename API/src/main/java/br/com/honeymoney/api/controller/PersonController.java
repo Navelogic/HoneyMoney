524a589b4e75205455b8f44bd28e724f426f2b2d
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-07 14:38:41 -0300

    Update PersonController.java

--- API/src/main/java/br/com/honeymoney/api/controller/PersonController.java
+++ API/src/main/java/br/com/honeymoney/api/controller/PersonController.java
@@ -1,9 +1,13 @@
 package br.com.honeymoney.api.controller;
 
+import br.com.honeymoney.api.model.Person;
 import br.com.honeymoney.api.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/persons")
@@ -17,4 +21,30 @@ public class PersonController {
         return "PersonController OK!";
     }
     // END Test
+
+    // CRUD methods
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return personService.findAll();
+    }
+
+    @GetMapping("/count")
+    public Integer count() {
+        return personService.count();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(Long id) {
+        return personService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody @Valid Person person, HttpServletResponse response) {
+        return personService.save(person, response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return personService.delete(id);
+    }
 }
