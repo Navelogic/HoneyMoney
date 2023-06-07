d35b210bf7a046315063a06652955541db33404c
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-06 21:56:28 -0300

    Update HoneyMoneyExceptionHandler.java

--- API/src/main/java/br/com/honeymoney/api/exceptionhandler/HoneyMoneyExceptionHandler.java
+++ API/src/main/java/br/com/honeymoney/api/exceptionhandler/HoneyMoneyExceptionHandler.java
@@ -2,9 +2,14 @@ package br.com.honeymoney.api.exceptionhandler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
@@ -14,6 +19,14 @@ public class HoneyMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        String userMessage = messageSource.getMessage("message.erro.category.toomanyatributes", null, LocaleContextHolder.getLocale());
+
+        return handleExceptionInternal(ex, userMessage, headers, HttpStatus.BAD_REQUEST, request);
+    }
 
     @ExceptionHandler(javax.validation.ConstraintViolationException.class)
     protected ResponseEntity<Object> handlerConstraintViolationException(javax.validation.ConstraintViolationException ex) {
