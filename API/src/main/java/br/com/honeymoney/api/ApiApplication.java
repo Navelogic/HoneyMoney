988d13b294c18f587414a65fa0fb75aff56b5102
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-06 21:56:41 -0300

    Update ApiApplication.java

--- API/src/main/java/br/com/honeymoney/api/ApiApplication.java
+++ API/src/main/java/br/com/honeymoney/api/ApiApplication.java
@@ -2,6 +2,10 @@ package br.com.honeymoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +14,11 @@ public class ApiApplication {
         SpringApplication.run(ApiApplication.class, args);
     }
 
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
