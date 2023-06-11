6eb1c10360a76b696206a9582fd28b04c9ba5ec6
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-11 15:04:37 -0300

    Update PicheController.java

--- API/src/main/java/br/com/honeymoney/api/controller/PicheController.java
+++ API/src/main/java/br/com/honeymoney/api/controller/PicheController.java
@@ -1,9 +1,13 @@
 package br.com.honeymoney.api.controller;
 
+import br.com.honeymoney.api.model.Piche;
 import br.com.honeymoney.api.service.PicheService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/piches")
@@ -18,4 +22,43 @@ public class PicheController {
         return "PicheController OK!";
     }
     // END Test
+
+    // CRUD
+
+    // CREATE
+    @PostMapping
+    public ResponseEntity<Piche> save(@RequestBody @Valid Piche piche, HttpServletResponse response) {
+        return picheService.save(piche, response);
+    }
+
+    // READ
+    @GetMapping("/{id}")
+    public ResponseEntity<Piche> findById(@PathVariable Long id) {
+        return picheService.findById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        return picheService.findAll();
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<Piche> update(@PathVariable Long id, @RequestBody @Valid Piche piche) {
+        return picheService.update(id, piche);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return picheService.delete(id);
+    }
+
+    // OTHER METHODS
+
+    // TOTAL PICHES
+    @GetMapping("/total")
+    public Integer count() {
+        return picheService.count();
+    }
 }
