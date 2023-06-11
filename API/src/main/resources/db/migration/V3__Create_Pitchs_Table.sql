dba3b934c484027ba3cd23ab5d10affc45029761
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-11 14:18:09 -0300

    Create V3__Create_Pitchs_Table.sql

---
+++ API/src/main/resources/db/migration/V3__Create_Pitchs_Table.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS tb_piches;
+CREATE TABLE tb_piches (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    description TEXT,
+    date_expiration DATE NOT NULL,
+    date_payment DATE,
+    price DECIMAL(10,2) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    id_category BIGINT UNSIGNED NOT NULL,
+    id_client BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (id_category) REFERENCES tb_categories(id),
+    FOREIGN KEY (id_client) REFERENCES tb_clients(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
