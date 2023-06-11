81d5e4f1090bba47cab46792e1ecd81e8511ed32
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-10 21:42:43 -0300

    Update User.java

--- API/src/main/java/br/com/honeymoney/api/model/User.java
+++ API/src/main/java/br/com/honeymoney/api/model/User.java
@@ -7,25 +7,30 @@ public class User {
     private Long id;
 
     // String
-    private String firstName;
-    private String lastName;
+    private String name;
     private String email;
     private String password;
-    private String address;
     private String phone;
-    private String title;
-    private String bio;
-    private String imageUrl;
-
-    // Boolean
-    private boolean enabled;
-    private boolean nonLocked;
-    private boolean usingMfa;
 
     // LocalDateTime
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private LocalDateTime lastLogin;
+
+    // Constructors
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, String phone, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime lastLogin) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.lastLogin = lastLogin;
+    }
 
     // Getters and Setters
     public Long getId() {
@@ -36,20 +41,12 @@ public class User {
         this.id = id;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getName() {
+        return name;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -68,68 +65,12 @@ public class User {
         this.password = password;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getPhone() {
         return phone;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public boolean isNonLocked() {
-        return nonLocked;
-    }
-
-    public void setNonLocked(boolean nonLocked) {
-        this.nonLocked = nonLocked;
-    }
-
-    public boolean isUsingMfa() {
-        return usingMfa;
-    }
-
-    public void setUsingMfa(boolean usingMfa) {
-        this.usingMfa = usingMfa;
     }
 
     public LocalDateTime getCreatedAt() {
