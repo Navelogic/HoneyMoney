eb038751588258d05be3aefe9934f8cdedcad933
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-11 23:39:52 -0300

    Update ResorceListener.java

--- API/src/main/java/br/com/honeymoney/api/event/listener/ResorceListener.java
+++ API/src/main/java/br/com/honeymoney/api/event/listener/ResorceListener.java
@@ -1,7 +1,6 @@
 package br.com.honeymoney.api.event.listener;
 
 import br.com.honeymoney.api.event.ResourceCreatedEvent;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -14,21 +13,24 @@ public class ResorceListener implements ApplicationListener<ResourceCreatedEvent
 
     @Override
     public void onApplicationEvent(ResourceCreatedEvent resourceCreatedEvent) {
+        // Obtém a resposta HTTP e o ID do recurso criado do evento
         HttpServletResponse response = resourceCreatedEvent.getResponse();
         Long id = resourceCreatedEvent.getId();
+
+        // Chama o método para adicionar o cabeçalho "Location" à resposta
         addHeaderLocation(response, id);
     }
 
-    private void addHeaderLocation(HttpServletResponse response, Long id){
-
+    // Adiciona o cabeçalho "Location" à resposta HTTP com o URI do recurso criado
+    private void addHeaderLocation(HttpServletResponse response, Long id) {
+        // Cria o URI usando o ServletUriComponentsBuilder, que extrai informações da requisição atual
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequestUri()
                 .path("/{id}")
                 .buildAndExpand(id)
                 .toUri();
+
+        // Define o cabeçalho "Location" na resposta com o valor do URI convertido em string ASCII
         response.setHeader("Location", location.toASCIIString());
     }
-
-
-
 }
