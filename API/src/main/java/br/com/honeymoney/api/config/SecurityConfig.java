7d30b592f56a612d4792f14a3a2d672252349f8f
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-12 00:36:03 -0300

    Update SecurityConfig.java

--- API/src/main/java/br/com/honeymoney/api/config/SecurityConfig.java
+++ API/src/main/java/br/com/honeymoney/api/config/SecurityConfig.java
@@ -1,4 +1,11 @@
 package br.com.honeymoney.api.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration // Indica que é uma classe de configuração
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
 }
