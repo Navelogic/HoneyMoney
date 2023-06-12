98b62438b8bb871c2d6f8fb2164eb1ea3e02df24
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-11 23:40:52 -0300

    Update ResourceCreatedEvent.java

--- API/src/main/java/br/com/honeymoney/api/event/ResourceCreatedEvent.java
+++ API/src/main/java/br/com/honeymoney/api/event/ResourceCreatedEvent.java
@@ -10,19 +10,19 @@ public class ResourceCreatedEvent extends ApplicationEvent {
     private HttpServletResponse response;
     private Long id;
 
-
-    // Constructors
+    // Construtor que recebe o objeto de origem, a resposta HTTP e o ID do recurso
     public ResourceCreatedEvent(Object source, HttpServletResponse response, Long id) {
         super(source);
         this.response = response;
         this.id = id;
     }
 
-    // Getters
+    // Getter para obter a resposta HTTP associada ao evento
     public HttpServletResponse getResponse() {
         return response;
     }
 
+    // Getter para obter o ID do recurso associado ao evento
     public Long getId() {
         return id;
     }
