c60ebe4032d50bd4c17bcb86b3dfaadce73700cd
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-11 23:58:01 -0300

    Create ClientNonexistentOuInactiveException.java

---
+++ API/src/main/java/br/com/honeymoney/api/service/exception/ClientNonexistentOuInactiveException.java
@@ -0,0 +1,5 @@
+package br.com.honeymoney.api.service.exception;
+
+public class ClientNonexistentOuInactiveException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
