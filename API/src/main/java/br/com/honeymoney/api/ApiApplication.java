bfd5f131b0745bbd9da2405f003344db30ade0d7
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-13 21:50:48 -0300

    Update ApiApplication.java

--- API/src/main/java/br/com/honeymoney/api/ApiApplication.java
+++ API/src/main/java/br/com/honeymoney/api/ApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-
-import java.io.IOException;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -16,9 +16,23 @@ public class ApiApplication {
 
     @Bean
     public ReloadableResourceBundleMessageSource messageSource() {
+        // Configura o MessageSource para carregar mensagens a partir do arquivo de resources "messages"
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        // Configura o CORS (Cross-Origin Resource Sharing) para permitir requisições de diferentes origens
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permite todas as origens
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"); // Define os métodos HTTP permitidos
+            }
+        };
     }
 }
